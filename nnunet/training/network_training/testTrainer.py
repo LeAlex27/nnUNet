f7d5ac3b36a29d82c39dad1332e45670186ba57c
Lennart Husvogt <lennart@husvogt.net>
2021-01-20 18:25:48 -0500

    Renamed to match filename.

--- nnunet/training/network_training/testTrainer.py
+++ nnunet/training/network_training/testTrainer.py
@@ -2,7 +2,7 @@ from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
 from nnunet.training.loss_functions.focal_loss import WeightedFocalLoss
 
 
-class TestTrainer(nnUNetTrainerV2):
+class testTrainer(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False, alpha=0.25, gamma=2):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
