cbedb8e93c0e02f7a8dcfa0ab57e5f92864c13e8
Lennart Husvogt <lennart@husvogt.net>
2021-01-20 23:22:32 -0500

    should run

--- nnunet/training/loss_functions/focal_loss.py
+++ nnunet/training/loss_functions/focal_loss.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 class FocalLoss(nn.Module):
     def __init__(self, alpha=.25, gamma=2):
         super(FocalLoss, self).__init__()
-        self.alpha = torch.tensor([alpha, 1-alpha]).cuda()
+        self.alpha = torch.tensor([alpha, 1 - alpha])  # .cuda()
         self.gamma = gamma
 
     def forward(self, inputs, targets):
@@ -14,5 +14,8 @@ class FocalLoss(nn.Module):
         targets = targets.type(torch.long)
         at = self.alpha.gather(0, targets.data.view(-1))
         pt = torch.exp(-BCE_loss)
-        F_loss = at*(1-pt)**self.gamma * BCE_loss
+
+        F_loss = at * (1 - pt.view(-1))
+        F_loss = F_loss ** self.gamma
+        F_loss *= BCE_loss.view(-1)
         return F_loss.mean()
