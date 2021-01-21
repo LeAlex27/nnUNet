69f81d127964c958c4e6c34cd512ddacaf6a015a
Lennart Husvogt <lennart@husvogt.net>
2021-01-21 12:58:58 -0500

    Reworked focal loss

--- nnunet/training/loss_functions/focal_loss.py
+++ nnunet/training/loss_functions/focal_loss.py
@@ -6,16 +6,20 @@ import torch.nn.functional as F
 class FocalLoss(nn.Module):
     def __init__(self, alpha=.25, gamma=2):
         super(FocalLoss, self).__init__()
-        self.alpha = torch.tensor([alpha, 1 - alpha]).cuda()
+        self.alpha = torch.tensor([alpha, 1.0 - alpha]).cuda()
         self.gamma = gamma
 
     def forward(self, inputs, targets):
+        # BCE(p, y) = -log(pt) => pt = exp(-BCE(p, y))
+        #      loss = -log(pt) * at(1-pt)**g
+        # =>        = BCE(p, y) * at * (1 - pt)**g
+
         BCE_loss = F.binary_cross_entropy_with_logits(inputs[:, 1:], targets, reduction='none')
+
         targets = targets.type(torch.long)
-        at = self.alpha.gather(0, targets.data.view(-1))
+        at = self.alpha.gather(0, targets.view(-1)).view(targets.size())
         pt = torch.exp(-BCE_loss)
 
-        F_loss = at * (1 - pt.view(-1))
-        F_loss = F_loss ** self.gamma
-        F_loss *= BCE_loss.view(-1)
+        F_loss = BCE_loss * at * (1 - pt) ** self.gamma
+
         return F_loss.mean()
