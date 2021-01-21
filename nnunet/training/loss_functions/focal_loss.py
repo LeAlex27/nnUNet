34ed63ff504226d6c4cbac1562d3ce8cd625050c
Lennart Husvogt <lennart@husvogt.net>
2021-01-20 19:08:58 -0500

    it's size, not shape

--- nnunet/training/loss_functions/focal_loss.py
+++ nnunet/training/loss_functions/focal_loss.py
@@ -11,14 +11,14 @@ class WeightedFocalLoss(nn.Module):
         self.gamma = gamma
 
     def forward(self, inputs, targets):
-        print("inputs.shape()", inputs.shape())
-        print("targets.shape()", targets.shape())
+        print("inputs.size()", inputs.size())
+        print("targets.size()", targets.size())
         BCE_loss = F.binary_cross_entropy_with_logits(inputs[:, 1:], targets, reduction='none')
-        print("BCE_loss.shape()", BCE_loss.shape())
+        print("BCE_loss.size()", BCE_loss.size())
         targets = targets.type(torch.long)
         at = self.alpha.gather(0, targets.data.view(-1))
-        print("at.shape()", at.shape())
+        print("at.size()", at.size())
         pt = torch.exp(-BCE_loss)
-        print("pt.shape()", pt.shape())
+        print("pt.size()", pt.size())
         F_loss = at*(1-pt)**self.gamma * BCE_loss
         return F_loss.mean()
