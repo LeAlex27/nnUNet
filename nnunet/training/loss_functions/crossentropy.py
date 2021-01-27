3f66590b926fbff6d640b86f37e4b99ff7db61bc
Lennart Husvogt <lennart@husvogt.net>
2021-01-27 18:34:20 -0500

    Added bceTrainer and weighted BCE

--- nnunet/training/loss_functions/crossentropy.py
+++ nnunet/training/loss_functions/crossentropy.py
@@ -10,3 +10,17 @@ class RobustCrossEntropyLoss(nn.CrossEntropyLoss):
             assert target.shape[1] == 1
             target = target[:, 0]
         return super().forward(input, target.long())
+
+
+class WeightedRobustCrossEntropyLoss(nn.CrossEntropyLoss):
+    def __init__(self, weight):
+        super(WeightedRobustCrossEntropyLoss, self).__init__(weight=Tensor(weight))
+    
+    """
+    this is just a compatibility layer because my target tensor is float and has an extra dimension
+    """
+    def forward(self, input: Tensor, target: Tensor) -> Tensor:
+        if len(target.shape) == len(input.shape):
+            assert target.shape[1] == 1
+            target = target[:, 0]
+        return super().forward(input, target.long())
