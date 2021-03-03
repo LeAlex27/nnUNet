30b8b446e733183229024e51007a78117437f5fa
Lennart Husvogt <lennart@husvogt.net>
2021-03-03 00:25:22 -0500

    debug output

--- nnunet/training/loss_functions/counting_dice_loss.py
+++ nnunet/training/loss_functions/counting_dice_loss.py
@@ -6,4 +6,7 @@ class CountingDiceLoss(SoftDiceLoss):
         super(CountingDiceLoss, self).__init__(**sd_kw)
 
     def forward(self, x, y, loss_mask=None):
+        print("cdLoss:")
+        print("\tx.shape:", x.shape)
+        print("\ty.shape:", y.shape)
         return 0
