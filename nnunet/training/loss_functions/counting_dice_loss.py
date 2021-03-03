b1b4f4d9863dbe6ec28349ac64ca78de479c295d
Lennart Husvogt <lennart@husvogt.net>
2021-03-03 02:38:11 -0500

    to gpu

--- nnunet/training/loss_functions/counting_dice_loss.py
+++ nnunet/training/loss_functions/counting_dice_loss.py
@@ -67,4 +67,4 @@ class CountingDiceLoss(SoftDiceLoss):
         print(sums_gt)
         print(sums_pred)
 
-        return super(CountingDiceLoss, self).forward(x, y_) + ma_loss
+        return super(CountingDiceLoss, self).forward(x, y_) + ma_loss.gpu()
