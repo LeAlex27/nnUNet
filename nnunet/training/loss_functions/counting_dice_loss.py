3f628c79b34ce0c1ce49f5de36816e4037194498
Lennart Husvogt <lennart@husvogt.net>
2021-03-17 13:35:33 -0400

    debug output, 10 epochs

--- nnunet/training/loss_functions/counting_dice_loss.py
+++ nnunet/training/loss_functions/counting_dice_loss.py
@@ -27,14 +27,15 @@ class CountingDiceLoss(torch.nn.Module):
         dm = torch.from_numpy(dm).cuda()
         y_n_ma = torch.sum(dm)
 
-        #l_ = self.loss(x[:, :-1], y) #, loss_mask=loss_mask)
-        #return l_
-        #print("loss:", l_)
-        #print(x[:, -1:].shape, dm.shape)
-        #l_ += self.loss_density_map(x[:, -1:], dm)
+        l_ = self.loss(x[:, :-1], y) #, loss_mask=loss_mask)
+        print("loss:", l_)
+
         print("sum x:", torch.sum(x[:, -1:]))
-        print("sum dm:", torch.sum(dm))
-        l_ = self.loss_density_map(x[:, -1:], dm)
+        print("sum dm:", y_n_ma)
+        print(x[:, -1:].shape, dm.shape)
+        t = self.loss_density_map(x[:, -1:], dm)
+        print("t:", t)
+        l_ += t
         print("loss + dm:", l_)
 
         #x_n_ma = torch.sum(x[:, -1:])
