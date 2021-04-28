b6fd58a5c8941471213ee019fd2a05ae7e9979ac
Lennart Husvogt <lennart@husvogt.net>
2021-04-28 18:56:15 -0400

    converted kw_args

--- nnunet/training/network_training/sawNewTrainer.py
+++ nnunet/training/network_training/sawNewTrainer.py
@@ -8,8 +8,10 @@ from nnunet.training.loss_functions.counting_dice_loss import CountingDiceLoss
 
 
 class sawNetTrainer(nnUNetTrainerV2):
-    def __init__(self, **kw_args):
-        super(sawNetTrainer, self).__init__(**kw_args)
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super(sawNetTrainer, self).__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
         self.loss = CountingDiceLoss()
         self.max_num_epochs = 10
 
