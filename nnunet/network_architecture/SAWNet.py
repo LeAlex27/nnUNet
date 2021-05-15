22fac98a5fabe31e28f0b9ceffb0af385eacef2c
Lennart Husvogt <lennart@husvogt.net>
2021-05-14 20:17:48 -0400

    try deep supervision

--- nnunet/network_architecture/SAWNet.py
+++ nnunet/network_architecture/SAWNet.py
@@ -52,7 +52,7 @@ class SAWNet(Generic_UNet):
                  feat_map_mul_on_downscale=2, conv_op=nn.Conv2d,
                  norm_op=nn.BatchNorm2d, norm_op_kwargs=None,
                  dropout_op=nn.Dropout2d, dropout_op_kwargs=None,
-                 nonlin=nn.LeakyReLU, nonlin_kwargs=None, deep_supervision=False, dropout_in_localization=False,
+                 nonlin=nn.LeakyReLU, nonlin_kwargs=None, deep_supervision=True, dropout_in_localization=False,
                  final_nonlin=softmax_helper, weightInitializer=InitWeights_He(1e-2), pool_op_kernel_sizes=None,
                  conv_kernel_sizes=None,
                  upscale_logits=False, convolutional_pooling=False, convolutional_upsampling=False,
