a44abbc4ffb80f544b815647cddd26e6ce75ecd4
Lennart Husvogt <lennart@husvogt.net>
2021-05-18 16:24:27 -0400

    sau-net clone

---
+++ nnunet/network_architecture/SAUNet.py
@@ -0,0 +1,62 @@
+from copy import deepcopy
+from nnunet.utilities.nd_softmax import softmax_helper
+from torch import nn
+import torch
+import numpy as np
+from nnunet.network_architecture.initialization import InitWeights_He
+from nnunet.network_architecture.neural_network import SegmentationNetwork
+from nnunet.network_architecture.SAWNet import SAUnit
+from nnunet.network_architecture.generic_UNet import Generic_UNet, ConvDropoutNormNonlin
+import torch.nn.functional
+
+
+class SAUNet(Generic_UNet):
+    def __init__(self, input_channels, base_num_features, num_classes, num_pool, num_conv_per_stage=2,
+                 feat_map_mul_on_downscale=2, conv_op=nn.Conv2d,
+                 norm_op=nn.BatchNorm2d, norm_op_kwargs=None,
+                 dropout_op=nn.Dropout2d, dropout_op_kwargs=None,
+                 nonlin=nn.LeakyReLU, nonlin_kwargs=None, deep_supervision=True, dropout_in_localization=False,
+                 final_nonlin=softmax_helper, weightInitializer=InitWeights_He(1e-2), pool_op_kernel_sizes=None,
+                 conv_kernel_sizes=None,
+                 upscale_logits=False, convolutional_pooling=False, convolutional_upsampling=False,
+                 max_num_features=None, basic_block=ConvDropoutNormNonlin,
+                 seg_output_use_bias=False):
+        super(SAUNet, self).__init__(input_channels, base_num_features, num_classes, num_pool, num_conv_per_stage,
+                 feat_map_mul_on_downscale, conv_op, norm_op, norm_op_kwargs,
+                 dropout_op, dropout_op_kwargs, nonlin, nonlin_kwargs, deep_supervision, dropout_in_localization,
+                 final_nonlin, weightInitializer, pool_op_kernel_sizes,
+                 conv_kernel_sizes, upscale_logits, convolutional_pooling, convolutional_upsampling,
+                 max_num_features, basic_block, seg_output_use_bias)
+
+        output_features = base_num_features
+        for d in range(num_pool):
+            input_features = output_features
+            output_features = int(np.round(output_features * feat_map_mul_on_downscale))
+            output_features = min(output_features, self.max_num_features)
+            print("{}: {}".format(d, input_features))
+
+        self.sau = SAUNet(output_features)
+
+    def forward(self, x):
+        skips = []
+        seg_outputs = []
+        for d in range(len(self.conv_blocks_context) - 1):
+            x = self.conv_blocks_context[d](x)
+            skips.append(x)
+            if not self.convolutional_pooling:
+                x = self.td[d](x)
+
+        x = self.conv_blocks_context[-1](x)
+        x = self.sau(x)
+
+        for u in range(len(self.tu)):
+            x = self.tu[u](x)
+            x = torch.cat((x, skips[-(u + 1)]), dim=1)
+            x = self.conv_blocks_localization[u](x)
+            seg_outputs.append(self.final_nonlin(self.seg_outputs[u](x)))
+
+        if self._deep_supervision and self.do_ds:
+            return tuple([seg_outputs[-1]] + [i(j) for i, j in
+                                              zip(list(self.upscale_logits_ops)[::-1], seg_outputs[:-1][::-1])])
+        else:
+            return seg_outputs[-1]
---
+++ nnunet/training/network_training/sauNetTrainer.py
@@ -0,0 +1,81 @@
+import torch
+from torch import nn
+from nnunet.training.network_training.nnUNetTrainerV2 import nnUNetTrainerV2
+from nnunet.network_architecture.SAWNet import SAWNet
+from nnunet.network_architecture.initialization import InitWeights_He
+from nnunet.training.loss_functions.counting_dice_loss import CountingDiceLoss
+import numpy as np
+
+
+class sauNetTrainer(nnUNetTrainerV2):
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super(sauNetTrainer, self).__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
+        self.loss = CountingDiceLoss(self.output_folder)
+        self.max_num_epochs = 350
+        self.initial_lr = 1e-3
+
+    def initialize_network(self):
+        """
+        - momentum 0.99
+        - SGD instead of Adam
+        - self.lr_scheduler = None because we do poly_lr
+        - deep supervision = True
+        - i am sure I forgot something here
+
+        Known issue: forgot to set neg_slope=0 in InitWeights_He; should not make a difference though
+        :return:
+        """
+        assert self.threeD is False
+        if self.threeD:
+            conv_op = nn.Conv3d
+            dropout_op = nn.Dropout3d
+            norm_op = nn.InstanceNorm3d
+
+        else:
+            conv_op = nn.Conv2d
+            dropout_op = nn.Dropout2d
+            norm_op = nn.InstanceNorm2d
+
+        norm_op_kwargs = {'eps': 1e-5, 'affine': True}
+        dropout_op_kwargs = {'p': 0, 'inplace': True}
+        net_nonlin = nn.LeakyReLU
+        net_nonlin_kwargs = {'negative_slope': 1e-2, 'inplace': True}
+        print("sawNetTrainer.py:46", self.net_num_pool_op_kernel_sizes)
+        self.network = SAWNet(self.num_input_channels, self.base_num_features, self.num_classes,
+                              len(self.net_num_pool_op_kernel_sizes),
+                              self.conv_per_stage, 2, conv_op, norm_op, norm_op_kwargs, dropout_op,
+                              dropout_op_kwargs,
+                              net_nonlin, net_nonlin_kwargs, True, False, lambda x: x, InitWeights_He(1e-2),
+                              self.net_num_pool_op_kernel_sizes, self.net_conv_kernel_sizes, False, True, True)
+        if torch.cuda.is_available():
+            self.network.cuda()
+        # self.network.inference_apply_nonlin = softmax_helper
+
+    def initialize_optimizer_and_scheduler(self):
+        assert self.network is not None, "self.initialize_network must be called first"
+        # self.optimizer = torch.optim.SGD(self.network.parameters(), self.initial_lr, weight_decay=self.weight_decay,
+        #                                  momentum=0.99, nesterov=True)
+        self.optimizer = torch.optim.Adam(self.network.parameters(), self.initial_lr)
+
+        def cosine_wwr(step):
+            t_mul = 2.0
+            m_mul = 1.0
+            alpha = 0.0
+            first_decay_steps = 50
+
+            global_step_recomp = step
+            completed_fraction = global_step_recomp / first_decay_steps
+
+            i_restart = np.floor(np.log(1.0 - completed_fraction * (1.0 - t_mul)) / np.log(t_mul))
+            sum_r = (1.0 - t_mul ** i_restart) / (1.0 - t_mul)
+            completed_fraction = (completed_fraction - sum_r) / t_mul ** i_restart
+
+            m_fac = m_mul ** i_restart
+            cosine_decayed = 0.5 * m_fac * (1.0 + np.cos(np.pi * completed_fraction))
+            decayed = (1 - alpha) * cosine_decayed + alpha
+
+            return decayed
+
+        self.lr_scheduler = torch.optim.lr_scheduler.LambdaLR(self.optimizer, cosine_wwr)
