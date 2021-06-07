74ebbae7b4ca60e6ce12ecbb551a451a0fafcd22
Lennart Husvogt <lennart@husvogt.net>
2021-06-07 18:26:03 -0400

    adam for 200 epochs no lr, but l_n

--- nnunet/training/network_training/sawNetTrainer.py
+++ nnunet/training/network_training/sawNetTrainer.py
@@ -13,10 +13,10 @@ class sawNetTrainer(nnUNetTrainerV2):
                  unpack_data=True, deterministic=True, fp16=False):
         super(sawNetTrainer, self).__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
                                             unpack_data, deterministic, fp16, False)
-        self.loss = CountingDiceLoss(label_loss=True, density_map_loss=True, count_loss=False,
+        self.loss = CountingDiceLoss(label_loss=True, density_map_loss=True, count_loss=True,
                                      output_folder=self.output_folder)
         self.optimizer = 'adam'
-        self.max_num_epochs = 100
+        self.max_num_epochs = 200
         self.initial_lr = 1e-3
         self.use_lr_scheduler = False
 
