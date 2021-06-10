79c9d2d610f3b793feaa141b8d28bff56b5a06c0
Lennart Husvogt <lennart@husvogt.net>
2021-06-10 10:07:18 -0400

    no lr scheduler 1e-4 for 100 epochs

--- nnunet/training/network_training/sawNetTrainer.py
+++ nnunet/training/network_training/sawNetTrainer.py
@@ -14,12 +14,12 @@ class sawNetTrainer(nnUNetTrainerV2):
         super(sawNetTrainer, self).__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
                                             unpack_data, deterministic, fp16, False)
         self.optimizer = 'adam'
-        self.max_num_epochs = 350
-        self.initial_lr = 5e-4
-        self.use_lr_scheduler = True
+        self.max_num_epochs = 100
+        self.initial_lr = 1e-4
+        self.use_lr_scheduler = False
         label_loss = True
         density_map_loss = True
-        count_loss = True
+        count_loss = False
 
         self.loss = CountingDiceLoss(label_loss=label_loss, density_map_loss=density_map_loss, count_loss=count_loss,
                                      output_folder=self.output_folder)
