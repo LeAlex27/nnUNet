ea5b5f5d5b28763f5c5744ec33055be65db876b3
Lennart Husvogt <lennart@husvogt.net>
2021-06-17 19:42:16 -0400

    Run for 150 epochs.

--- nnunet/training/network_training/sawNetTrainerMultiOpts.py
+++ nnunet/training/network_training/sawNetTrainerMultiOpts.py
@@ -20,7 +20,7 @@ class sawNetTrainerMultiOpts(nnUNetTrainerV2):
                  unpack_data=True, deterministic=True, fp16=False):
         super(sawNetTrainerMultiOpts, self).__init__(plans_file, fold, output_folder, dataset_directory, batch_dice,
                                                      stage, unpack_data, deterministic, fp16, False)
-        self.max_num_epochs = 300
+        self.max_num_epochs = 150
         self.loss = None
         self.opt_loss = []
 
