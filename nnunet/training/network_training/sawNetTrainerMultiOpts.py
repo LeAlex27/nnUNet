dd62c9a38d2a7eefe7e6f98bd7b1ffc08329c21c
Lennart Husvogt <lennart@husvogt.net>
2021-06-24 19:18:14 -0400

    debug test

--- nnunet/training/network_training/sawNetTrainerMultiOpts.py
+++ nnunet/training/network_training/sawNetTrainerMultiOpts.py
@@ -44,12 +44,13 @@ class DensityMapTransform(AbstractTransform):
     def __call__(self, **data_dict):
         print("sawNetTrainerMultiOpts.py:46")
         breakpoint()
-        y = data_dict['seg']
-        new_y = np.empty((y.shape[0], y.shape[1] + 1, y.shape[2], y.shape[3]), dtype=y.dtype)
-        new_y[:, :1] = y
-        for i in range(y.shape[0]):
-            new_y[i, 1] = CountingDiceLoss.sharpen(y[i, 0])
-        data_dict['seg'] = new_y
+        print(data_dict.keys())
+        #y = data_dict['seg']
+        #new_y = np.empty((y.shape[0], y.shape[1] + 1, y.shape[2], y.shape[3]), dtype=y.dtype)
+        #new_y[:, :1] = y
+        #for i in range(y.shape[0]):
+        #    new_y[i, 1] = CountingDiceLoss.sharpen(y[i, 0])
+        #data_dict['seg'] = new_y
         return data_dict
 
 
