41e4c42ca8c432752bd8449f774751fcab74d633
Lennart Husvogt <lennart@husvogt.net>
2021-07-08 12:10:02 -0400

    run dice and nnUNet trainers without DS for 500 epochs

--- nnunet/training/network_training/nnUNetTrainerV2.py
+++ nnunet/training/network_training/nnUNetTrainerV2.py
@@ -42,7 +42,7 @@ class nnUNetTrainerV2(nnUNetTrainer):
     """
 
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
-                 unpack_data=True, deterministic=True, fp16=False, deep_supervision=True):
+                 unpack_data=True, deterministic=True, fp16=False, deep_supervision=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
         self.max_num_epochs = 500
