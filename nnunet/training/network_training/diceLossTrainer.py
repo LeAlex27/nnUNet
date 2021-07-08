604578085f889415f8c05af212f5693117b4ddcf
Lennart Husvogt <lennart@husvogt.net>
2021-07-08 09:56:25 -0400

    tru nnUNetTrainerV2 with DS

--- nnunet/training/network_training/diceLossTrainer.py
+++ nnunet/training/network_training/diceLossTrainer.py
@@ -7,7 +7,7 @@ class diceLossTrainer(nnUNetTrainerV2):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
-                         deterministic, fp16, deep_supervision=False)
+                         deterministic, fp16, deep_supervision=True)
         soft_dice_kwargs = {'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False}
-        self.loss = SoftDiceLoss(apply_nonlin=softmax_helper, **soft_dice_kwargs)
+        self.loss = SoftDiceLoss(**soft_dice_kwargs)
         self.epoch = 1000
